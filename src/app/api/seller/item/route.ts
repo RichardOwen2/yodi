0bc27a6bc892342db9b0b1d1390b66307ea81f8d
unknown <richardowen2411@gmail.com>
2023-05-01 12:13:58 +0800

    refractor api route

--- src/app/api/seller/item/route.ts
+++ src/app/api/seller/item/route.ts
@@ -4,8 +4,12 @@ import getTokenHandler from "@/backend/utils/getTokenHandler";
 import errorHandler from "@/backend/utils/errorHandler";
 
 import verifySellerAccess from "@/backend/services/seller";
-import { getItemsBySeller, getVerifiedItemsBySeller } from "@/backend/services/seller/itemService";
+import { validatePostItemPayload } from "@/backend/validators/seller/itemValidator";
+
 import getPaginationParams from "@/backend/utils/getPaginationParams";
+
+import { getItemsBySeller, getVerifiedItemsBySeller } from "@/backend/services/seller/itemService";
+import { addItem } from "@/backend/services/seller/itemService";
 
 export async function GET(request: Request) {
   try {
@@ -46,3 +50,34 @@ export async function GET(request: Request) {
   }
 }
 
+export async function POST(request: Request) {
+  try {
+    const userId = getTokenHandler(request);
+    await verifySellerAccess(userId);
+
+    const body = await request.json();
+    validatePostItemPayload(body);
+
+    const {
+      title,
+      description = "",
+      image,
+      price,
+      stock,
+    } = body;
+
+    await addItem(userId, { title, description, image, price, stock });
+
+    return NextResponse.json({
+      status: "success",
+      message: "Berhasil mengajukan item penjualan",
+    }, { status: 201 });
+  } catch (error) {
+    const { data, status } = errorHandler(error);
+
+    return NextResponse.json({
+      status: data.status,
+      message: data.message,
+    }, { status });
+  }
+}
