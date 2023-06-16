c6e21f449fb869195afc8030009775a5650a2a71
unknown <richardowen2411@gmail.com>
2023-06-17 01:36:00 +0800

    fixing apk2

--- src/pages/api/app.js
+++ src/pages/api/app.js
@@ -5,15 +5,15 @@ export default function handler(req, res) {
   // Get the file path relative to the public folder
   const filePath = path.join(process.cwd(), 'public', 'app.apk');
 
-  // Read the file from the local filesystem
-  const file = fs.readFileSync(filePath);
-
   // Set the response headers to indicate a downloadable APK file
   res.setHeader('Content-Type', 'application/vnd.android.package-archive');
-  res.setHeader('Content-Disposition', 'attachment; filename="file.apk"');
+  res.setHeader('Content-Disposition', 'attachment; filename="largeFile.apk"');
 
-  // Send the file content as the API response
-  res.send(file);
+  // Create a read stream for the APK file
+  const stream = fs.createReadStream(filePath);
+
+  // Stream the APK file content to the response
+  stream.pipe(res);
 }
 
 export const config = {
