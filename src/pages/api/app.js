45e640286547e6761a3794bdc9c456959ffb41bc
unknown <richardowen2411@gmail.com>
2023-06-17 02:07:42 +0800

    test9

--- src/pages/api/app.js
+++ src/pages/api/app.js
@@ -1,23 +1,39 @@
 import fs from 'fs';
 import path from 'path';
 
-export default function handler(req, res) {
-  // Get the file path relative to the public folder
+export default async function handler(req, res) {
   const filePath = path.join(process.cwd(), 'public', 'app.apk');
+
+  const chunkSize = 1024 * 1024; // 1MB chunk size
+
+  const fileStats = fs.statSync(filePath);
+  const fileSize = fileStats.size;
+
+  // Calculate the number of chunks needed
+  const numChunks = Math.ceil(fileSize / chunkSize);
 
   // Set the response headers to indicate a downloadable APK file
   res.setHeader('Content-Type', 'application/vnd.android.package-archive');
   res.setHeader('Content-Disposition', 'attachment; filename="yodi.apk"');
 
-  // Create a readable stream for the APK file
-  const fileStream = fs.createReadStream(filePath);
+  for (let chunkIndex = 0; chunkIndex < numChunks; chunkIndex++) {
+    // Calculate the start and end positions for the current chunk
+    const start = chunkIndex * chunkSize;
+    const end = Math.min(start + chunkSize, fileSize);
 
-  // Enable chunked encoding for progressive response
-  res.setHeader('Transfer-Encoding', 'chunked');
+    // Create a readable stream for the current chunk
+    const fileStream = fs.createReadStream(filePath, { start, end });
 
-  // Stream the file content to the response in chunks
-  fileStream.pipe(res);
+    // Stream the chunk content to the response
+    await new Promise((resolve) => {
+      fileStream.pipe(res, { end: false });
+      fileStream.on('end', resolve);
+    });
+  }
+
+  res.end();
 }
+
 export const config = {
   api: {
     responseLimit: false,
